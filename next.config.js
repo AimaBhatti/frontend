5255e73e6f2012afae7f42bd2637e7168eaaa39f
Aima Bhatti <aimaadnan00@gmail.com>
2022-10-24 18:55:00 +0500

    error

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  images: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    loader: "default",
+    domains: [process.env.IMAGES_DOMAIN],
+  },
 }
 
 module.exports = nextConfig
