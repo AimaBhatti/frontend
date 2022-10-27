35e41b9a6884631358c0378224691eafae750d4f
Aima Bhatti <aimaadnan00@gmail.com>
2022-10-28 00:08:24 +0500

    ..

--- pages/profile.js
+++ pages/profile.js
@@ -1,14 +1,16 @@
-import Head from 'next/head'
-import React from 'react'
-import Link from 'next/link'
-import { fetcher } from '../lib/api';
-import { getTokenFromServerCookie } from '../lib/auth';
+import Head from "next/head";
+import React from "react";
+import Link from "next/link";
+import { fetcher } from "../lib/api";
+import { getTokenFromServerCookie } from "../lib/auth";
 
 export async function getServerSideProps({ req }) {
   const jwt = getTokenFromServerCookie(req);
   // const id = getIdFromServerCookie(req);
-  let headers = { Authorization: `Bearer ${jwt}` }
-  let user = await fetcher(`http://localhost:1337/api/users/me`, { headers: headers });
+  let headers = { Authorization: `Bearer ${jwt}` };
+  let user = await fetcher(`http://localhost:1337/api/users/me`, {
+    headers: headers,
+  });
   return {
     props: { user },
   };
@@ -31,55 +33,104 @@ export default function profile({ user }) {
               <div className="row mt-2">
                 <div className="col-md-6">
                   <label className="labels">Name</label>
-                  <input type="text" className="form-control" value={user.username} disabled />
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={user.username}
+                    disabled
+                  />
                 </div>
                 <div className="col-md-6">
                   <label className="labels">Gender</label>
-                  <input type="text" className="form-control" value={user.gender} disabled />
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={user.gender}
+                    disabled
+                  />
                 </div>
                 <div className="col-md-6">
                   <label className="labels">Mobile Number</label>
-                  <input type="number" className="form-control" value={user.contact} disabled />
+                  <input
+                    type="number"
+                    className="form-control"
+                    value={user.contact}
+                    disabled
+                  />
                 </div>
                 <div className="col-md-9">
                   <label className="labels">Email</label>
-                  <input type="email" className="form-control" value={user.email} disabled />
+                  <input
+                    type="email"
+                    className="form-control"
+                    value={user.email}
+                    disabled
+                  />
                 </div>
               </div>
 
               <div className="row mt-3">
                 <div className="col-md-12">
                   <label className="labels">Address</label>
-                  <input type="text" className="form-control" value={user.address} disabled />
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={user.address}
+                    disabled
+                  />
                 </div>
                 <div className="col-md-6">
                   <label className="labels">Area</label>
-                  <input type="text" className="form-control" value={user.area} disabled />
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={user.area}
+                    disabled
+                  />
                 </div>
                 <div className="col-md-6">
                   <label className="labels">City</label>
-                  <input type="text" className="form-control" value={user.city} disabled />
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={user.city}
+                    disabled
+                  />
                 </div>
                 <div className="col-md-6">
                   <label className="labels">State</label>
-                  <input type="text" className="form-control" value={user.state} disabled />
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={user.state}
+                    disabled
+                  />
                 </div>
                 <div className="col-md-6">
                   <label className="labels">Country</label>
-                  <input type="text" className="form-control" value={user.country} disabled />
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={user.country}
+                    disabled
+                  />
                 </div>
               </div>
 
               <div className="mt-5 text-center">
-                <Link href='/setProfile'><button className="btn btn-primary profile-button" type="button">Update Profile</button></Link>
-                <button className="btn btn-primary profile-button" type="button">Reset Password</button>
-                <button className="btn btn-primary profile-button" type="submit">Delete Account</button>
+                <Link href="/setProfile">
+                  <button
+                    className="btn btn-primary profile-button"
+                    type="button"
+                  >
+                    Update Profile
+                  </button>
+                </Link>
               </div>
             </div>
-
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
